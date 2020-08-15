3d2bd1613bb679433384026595bc58117df5990c
Trung DUONG VIET <dg.viettrung@gmail.com>
2020-08-15 22:33:43 +0700

    add api GET /books

--- rest-api-express/server.js
+++ rest-api-express/server.js
@@ -1,7 +1,16 @@
 const express = require('express')
-const app = express()
 
+const app = express()
+const bookRouter = express.Router()
 const PORT = process.env.PORT || 8080
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = {hello: 'This is my API'}
+    res.json(response)
+  })
+
+app.use('/api', bookRouter)
 
 app.get('/', (req, res) => {
   res.send(`
